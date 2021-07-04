258404640e15c40c5c220e9de4d93fa4eaa38cba
Jayfunf <simh3077@gmail.com>
2021-07-05 01:05:08 +0900

    solved

--- 예산.cpp
+++ 예산.cpp
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> d, int budget)
 {
-    int answer = 0;
+    int answer=0;
+    int sum=0;
+    sort(d.begin(),d.end());
+    for(int i = 0; i<d.size();i++){
+        sum+=d[i];
+        if(sum>budget){
+            answer = i;
+            break;
+        }
+        else
+            answer = i+1;
+    }
     return answer;
 }
