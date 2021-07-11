0e466c70da1dc8297a2d8a936af953eb47c16c11
Jayfunf <simh3077@gmail.com>
2021-07-11 19:54:07 +0900

    solved

--- 자릿수_더하기.cpp
+++ 자릿수_더하기.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
-    vector<int> arr;
-    
+    int num = 0;
 
+    while(n > 0){
+        num = n%10;
+        answer += num;
+        n = n/10;
+    }
     return answer;
 }
