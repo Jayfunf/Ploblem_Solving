d6f314ff55f22ee74f658d028fb5b64d785284ba
Jayfunf <simh3077@gmail.com>
2021-07-12 18:00:53 +0900

    solved

---
+++ 더_맵게.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K)
+{
+    int answer = 0;
+    int temp1, temp2;
+    priority_queue<int,vector<int>,greater<int>> sco;
+    for(int i = 0; i< scoville.size();i++){
+        sco.push(scoville[i]);
+    }
+    while((sco.top()<K)&&(sco.size()>1)){
+        temp1=sco.top();
+        sco.pop();
+        temp2=sco.top();
+        sco.pop();
+        sco.push(temp1+(temp2*2));
+        answer++;
+    }
+    if(sco.top()<K)
+        return -1;
+    return answer;
+}
+//참고 블로그 https://coding-soomae.tistory.com/11
+//참고 블로그 https://blog.naver.com/smkim104/222407594965
