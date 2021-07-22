925436b51266721d4ae721b3fca5c71e2c942217
Jayfunf <simh3077@gmail.com>
2021-07-22 21:00:32 +0900

    solved

--- 문자열_내_마음대로_정렬하기.cpp
+++ 문자열_내_마음대로_정렬하기.cpp
@@ -1,17 +1,20 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
+
+int Num;
+
+bool compare(string a, string b){
+    return a.at(Num) == b.at(Num) ? a < b : a.at(Num) < b.at(Num);
+}
 
 vector<string> solution(vector<string> strings, int n)
 {
     vector<string> answer = strings;
-    vector<string> tmp;
-    for(int i = 0; i<n ; i++){
-        tmp[i] = answer[i][n];
-    }
-    sort(tmp.begin(), tmp.end());
-
+    Num = n;
+    sort(answer.begin(),answer.end(),compare);
     return answer;
 }
