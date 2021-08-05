fafe09152ee69fee4f7df8ff9b0e6f71c1e47f0d
Jayfunf <simh3077@gmail.com>
2021-08-06 07:01:29 +0900

    sloved

--- test.cpp
+++ test.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
-#include <algorithm>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int main(){
-    int a = 5;
-    int b = 3;
+int solution(vector<int> priorities, int location)
+{
+    int answer = 0;
 
-    cout << a << b <<endl;
-    a ^= b ^= a ^= b;
-    cout << a << b << endl;
+    queue<pair<int,int>> que;
+    priority_queue<int> pri_que; //내림 정렬
 
-    return 0;
+    for(int i = 0; i< priorities.size();i++){
+        que.push(make_pair(i,priorities[i]));
+        pri_que.push(priorities[i]);
+    }
+    while(!que.empty()){
+        int index = que.front().first;
+        int value = que.front().second;
+        que.pop();
+        if(value == pri_que.top()){
+            pri_que.pop();
+            answer++;
+            if(index == location)
+                break;
+        }
+        else
+            que.push(make_pair(index,value));
+    }
+    return answer;
 }
